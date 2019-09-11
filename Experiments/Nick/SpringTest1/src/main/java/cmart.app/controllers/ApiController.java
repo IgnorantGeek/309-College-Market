05c226bfe9d51be05111dc18955e95901468a9a7
Nick Heisler <nheisler@iastate.edu>
2019-09-10 20:23:38 -0500

    moving to new test project

--- Experiments/Nick/SpringTest1/src/main/java/cmart.app/controllers/ApiController.java
+++ Experiments/Nick/SpringTest1/src/main/java/cmart.app/controllers/ApiController.java
@@ -3,6 +3,7 @@ package cmart.app.controllers;
 import java.util.Random;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -15,7 +16,7 @@ import cmart.app.classes.*;
 @RequestMapping("/api")
 public class ApiController
 {
-    @RequestMapping("")
+    @RequestMapping(value = "", method = RequestMethod.GET)
     public String home()
     {
         return "Mobile API Entry, go to /api/testUser to see sample JSON.";
