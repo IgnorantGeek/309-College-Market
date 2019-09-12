6fe814fd33f2ca13bfe2409c018217bf84cf9ed3
Nick Heisler <nheisler@iastate.edu>
2019-09-12 00:47:45 -0500

    Added connection to mySQL database

--- Experiments/Nick/SpringTest2/app/src/main/java/org/collegemarket/app/Application.java
+++ Experiments/Nick/SpringTest2/app/src/main/java/org/collegemarket/app/Application.java
@@ -2,7 +2,9 @@ package org.collegemarket.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories(basePackages = "org.collegemarket.db.repositories")
 @SpringBootApplication
 public class Application
 {
