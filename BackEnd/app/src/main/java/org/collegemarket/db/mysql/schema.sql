1130a5e7bbaad7f01035a39db4b934cf8d8e971e
Fadel Alshammasi <fadelalshammasi@Fadels-MacBook-Pro.local>
2019-09-17 17:40:07 -0500

    modifications

--- BackEnd/app/src/main/java/org/collegemarket/db/mysql/schema.sql
+++ BackEnd/app/src/main/java/org/collegemarket/db/mysql/schema.sql
@@ -1,9 +1,9 @@
 CREATE TABLE IF NOT EXISTS users(
     id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     user_name  VARCHAR(20) NOT NULL,
-    password   VARCHAR(30) NOT NULL,
-    first_name VARCHAR(30),
-    last_name  VARCHAR(30)
+    password   VARCHAR(16) NOT NULL,
+    first_name VARCHAR(20),
+    last_name  VARCHAR(20)
 );
 CREATE TABLE IF NOT EXISTS admins(
     id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
