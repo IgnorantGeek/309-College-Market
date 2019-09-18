c5cf035bd15162ca7b893f8f0bf07b19baa00009
Lily Krohn <Lily@lilykrohn.student.iastate.edu>
2019-09-18 14:13:15 -0500

    Connected login and register pages

--- Experiments/Lily/LoginExperiment/app/src/main/java/com/example/loginexperiment/UserActivity.java
+++ Experiments/Lily/LoginExperiment/app/src/main/java/com/example/loginexperiment/UserActivity.java
@@ -3,6 +3,8 @@ package com.example.loginexperiment;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class UserActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class UserActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
+
+
+        final EditText etUsername = (EditText) findViewById(R.id.etUsername);
+        final EditText etUniversity = (EditText) findViewById(R.id.etUniversity);
+
+        final TextView welcomeMessage = (TextView) findViewById(R.id.tvWelcomeMsg);
+
+
+
     }
 }
