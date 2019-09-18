5f9f3698f4a049496238de33bd273286beacad72
Fadel <fadelsh@iastate.edu>
2019-09-18 21:42:44 +0000

    added email & university fields

--- BackEnd/app/src/main/java/org/collegemarket/db/mysql/schema.sql
+++ BackEnd/app/src/main/java/org/collegemarket/db/mysql/schema.sql
@@ -3,7 +3,9 @@ CREATE TABLE IF NOT EXISTS users(
     user_name  VARCHAR(20) NOT NULL,
     password   VARCHAR(16) NOT NULL,
     first_name VARCHAR(20),
-    last_name  VARCHAR(20)
+    last_name  VARCHAR(20),
+    university VARCHAR (30),
+    email VARCHAR (35)
 );
 CREATE TABLE IF NOT EXISTS admins(
     id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
