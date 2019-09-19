c8f1dc7214e45ec6a269e58e6b0cb3906f239186
Katelyn <klamison@iastate.edu>
2019-09-18 22:47:28 -0500

    renaming from loginexperiment to CampusMarket

--- FrontEnd/campusMarket/app/src/main/java/com/example/CampusMarket/LoginActivity.java
+++ FrontEnd/campusMarket/app/src/main/java/com/example/CampusMarket/LoginActivity.java
@@ -1,4 +1,4 @@
-package com.example.loginexperiment;
+package com.example.CampusMarket;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,6 +8,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.example.loginexperiment.R;
 
 public class LoginActivity extends AppCompatActivity {
 
--- FrontEnd/campusMarket/app/src/main/java/com/example/CampusMarket/RegisterActivity.java
+++ FrontEnd/campusMarket/app/src/main/java/com/example/CampusMarket/RegisterActivity.java
@@ -1,10 +1,12 @@
-package com.example.loginexperiment;
+package com.example.CampusMarket;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.example.loginexperiment.R;
 
 public class RegisterActivity extends AppCompatActivity {
 
