5b8667b3ef3c7e16e9a394c79f0494c9b5a7bb55
Katelyn <klamison@iastate.edu>
2019-09-25 16:53:54 -0500

    finally got volley requests working. added extra request types

--- FrontEnd/cMarket/app/src/main/java/com/example/campusmarket/JsonRequestActivity.java
+++ FrontEnd/cMarket/app/src/main/java/com/example/campusmarket/JsonRequestActivity.java
@@ -5,44 +5,50 @@ import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
 
-import com.android.volley.RequestQueue;
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request.Method;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.VolleyLog;
 import com.android.volley.toolbox.JsonArrayRequest;
-import com.android.volley.toolbox.Volley;
-import com.example.campusmarket.app.MyVolley;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.example.campusmarket.app.AppController;
 import com.example.campusmarket.utils.Const;
 
 import org.json.JSONArray;
+import org.json.JSONObject;
 
-public class JsonRequestActivity extends Activity implements View.OnClickListener {
+import java.util.HashMap;
+import java.util.Map;
+
+public class JsonRequestActivity extends Activity implements OnClickListener {
 
     private String TAG = JsonRequestActivity.class.getSimpleName();
-    private Button btnJsonArray;
+    private Button btnJsonObj, btnJsonArray;
     private TextView msgResponse;
     private ProgressDialog pDialog;
-    private RequestQueue queue;
 
     // These tags will be used to cancel the requests
-    private String  tag_json_arry = "jarray_req";
+    private String tag_json_obj = "jobj_req", tag_json_arry = "jarray_req";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.json_request);
 
+        btnJsonObj = (Button) findViewById(R.id.btnJsonObj);
         btnJsonArray = (Button) findViewById(R.id.btnJsonArray);
         msgResponse = (TextView) findViewById(R.id.msgResponse);
-        queue = Volley.newRequestQueue(this);
 
         pDialog = new ProgressDialog(this);
         pDialog.setMessage("Loading...");
         pDialog.setCancelable(false);
 
+        btnJsonObj.setOnClickListener(this);
         btnJsonArray.setOnClickListener(this);
     }
 
@@ -56,6 +62,59 @@ public class JsonRequestActivity extends Activity implements View.OnClickListene
             pDialog.hide();
     }
 
+    /**
+     * Making json object request
+     * */
+    private void makeJsonObjReq() {
+        showProgressDialog();
+        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Method.GET,
+                Const.URL_JSON_OBJECT, null,
+                new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d(TAG, response.toString());
+                        msgResponse.setText(response.toString());
+                        hideProgressDialog();
+                    }
+                }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d(TAG, "Error: " + error.getMessage());
+                hideProgressDialog();
+            }
+        }) {
+
+            /**
+             * Passing some request headers
+             * */
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<String, String>();
+                headers.put("Content-Type", "application/json");
+                return headers;
+            }
+
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("name", "Androidhive");
+                params.put("email", "abc@androidhive.info");
+                params.put("pass", "password123");
+
+                return params;
+            }
+
+        };
+
+        // Adding request to request queue
+        AppController.getInstance().addToRequestQueue(jsonObjReq,
+                tag_json_obj);
+
+        // Cancelling request
+        // ApplicationController.getInstance().getRequestQueue().cancelAll(tag_json_obj);
+    }
 
     /**
      * Making json array request
@@ -79,12 +138,8 @@ public class JsonRequestActivity extends Activity implements View.OnClickListene
         });
 
         // Adding request to request queue
-//        AppController.getInstance().addToRequestQueue(req,
-//                tag_json_arry);
-
-        // Adding request to request queue
-        queue.add(req);
-        //MyVolley.getInstance(this).addToRequestQueue(req, tag_json_arry);
+        AppController.getInstance().addToRequestQueue(req,
+                tag_json_arry);
 
         // Cancelling request
         // ApplicationController.getInstance().getRequestQueue().cancelAll(tag_json_arry);
@@ -93,12 +148,14 @@ public class JsonRequestActivity extends Activity implements View.OnClickListene
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
+            case R.id.btnJsonObj:
+                makeJsonObjReq();
+                break;
             case R.id.btnJsonArray:
                 makeJsonArryReq();
                 break;
         }
 
     }
-
 
 }
--- FrontEnd/cMarket/app/src/main/java/com/example/campusmarket/utils/Const.java
+++ FrontEnd/cMarket/app/src/main/java/com/example/campusmarket/utils/Const.java
@@ -1,6 +1,9 @@
 package com.example.campusmarket.utils;
 
 public class Const {
-    public static final String URL_JSON_ARRAY = "https://api.androidhive.info/volley/person_array.json";
-//    public static final String URL_JSON_ARRAY = "http://coms-309-jr-1.misc.iastate.edu:8080/users/all";
+    public static final String URL_JSON_OBJECT = "http://coms-309-jr-1.misc.iastate.edu:8080/users/all";
+    public static final String URL_JSON_ARRAY = "http://coms-309-jr-1.misc.iastate.edu:8080/users/all";
+//     public static final String URL_JSON_ARRAY = "https://api.androidhive.info/volley/person_array.json";
+    public static final String URL_STRING_REQ = "http://coms-309-jr-1.misc.iastate.edu";
+    public static final String URL_IMAGE = "https://api.androidhive.info/volley/volley-image.jpg";
 }
