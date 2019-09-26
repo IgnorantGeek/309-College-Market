c3352ae060989468422f298dcf300a8b2f946632
Lily Krohn <Lily@admins-MacBook-Pro-2.local>
2019-09-26 08:41:11 -0500

    Started working on some registration validation

--- FrontEnd/cMarket/app/src/main/java/com/example/campusmarket/RegisterActivity.java
+++ FrontEnd/cMarket/app/src/main/java/com/example/campusmarket/RegisterActivity.java
@@ -7,8 +7,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.text.TextUtils;
+import android.util.Patterns;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    EditText etFirstName;
+    EditText etLastName;
+    EditText etUsername;
+    EditText etPassword;
+    EditText etUniversity;
+    EditText etEmail;
+    Button bRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,22 +26,64 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
 
 
-        final EditText etFirstName = (EditText) findViewById(R.id.etFirstName);
-        final EditText etLastName = (EditText) findViewById(R.id.etLastName);
-        final EditText etUsername = (EditText) findViewById(R.id.etUsername);
-        final EditText etPassword = (EditText) findViewById(R.id.etPassword);
-        final EditText etUniversity = (EditText) findViewById(R.id.etUniversity);
-        final EditText etEmail = (EditText) findViewById(R.id.etEmail);
+        etFirstName = findViewById(R.id.etFirstName);
+        etLastName = findViewById(R.id.etLastName);
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+        etUniversity = findViewById(R.id.etUniversity);
+        etEmail = findViewById(R.id.etEmail);
 
-        final Button bRegister = (Button) findViewById(R.id.bRegister);
+        bRegister = findViewById(R.id.bRegister);
+
     }
 
-    // Called when user finishes signing up
-    public void finishSignUp(View view) {
-        Intent intent = new Intent(this, UserActivity.class);
-        EditText editText = (EditText) findViewById(R.id.etUsername);
-        String message = editText.getText().toString();
-        intent.putExtra(MainActivity.EXTRA_MESSAGE, message);
-        startActivity(intent);
+        // Called when user finishes signing up
+        public void finishSignUp (View view){
+            Intent intent = new Intent(this, UserActivity.class);
+            EditText editText = (EditText) findViewById(R.id.etUsername);
+            String message = editText.getText().toString();
+            intent.putExtra(MainActivity.EXTRA_MESSAGE, message);
+            startActivity(intent);
+
+        }
     }
-}
+
+
+
+//        // Starts the button's onCLick validation process when button is clicked
+//        bRegister.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                checkDataEntry();
+//        }
+//    });
+//}
+
+//    // Checks for valid email address entry
+//    boolean isEmail(EditText text) {
+//        CharSequence etEmail = text.getText().toString();
+//        return (!TextUtils.isEmpty(etEmail) && Patterns.EMAIL_ADDRESS.matcher(etEmail).matches());
+//    }
+//
+//    // Checks for empty field / no entry
+//    boolean isEmpty(EditText text) {
+//        CharSequence str = text.getText().toString();
+//        return TextUtils.isEmpty(str);
+//    }
+
+//    // Goes through each implemented field and determines validity - then prints a message if invalid
+//    void checkDataEntry() {
+//        if (isEmpty(etFirstName)) {
+//            etFirstName.setError("Enter first name");
+//        }
+//
+//        if (isEmpty(etLastName)) {
+//            etLastName.setError("Last name is required");
+//        }
+//
+//        if (isEmail(etEmail) == false) {
+//            etEmail.setError("Enter a valid email");
+//        }
+//
+//    }
+
