222fb99c79f87403c74a2f13217613b3dd8fb66c
nheisler <nheisler@iastate.edu>
2019-09-27 03:50:15 +0000

    Added admin boolean flag to User class

--- BackEnd/app/src/main/java/org/campusmarket/app/models/User.java
+++ BackEnd/app/src/main/java/org/campusmarket/app/models/User.java
@@ -2,13 +2,7 @@ package org.campusmarket.app.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "users")
@@ -32,15 +26,36 @@ public class User
     private String  email;
     @Column(name = "university")
     private String  university;
+    @Column(name = "admin")
+    private boolean admin;
 
+    /*--- Links to Other Repositories ---*/
     @OneToOne(mappedBy = "user")
     private Session session;
+
     
     /*--- Constructors ---*/
     public User() { }
-    // Do we need more constructors?
 
-    /*--- Getters/Setters ---*/
+    public User(String username, 
+                String password,
+                String firstname,
+                String lastname,
+                String email,
+                String university,
+                boolean admin)
+    {
+        this.username = username;
+        this.password = password;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+        this.university = university;
+        this.admin = admin;
+    }
+    
+
+    /*--- Getter Methods ---*/
     public String getUsername()
     {
         return this.username;
@@ -69,6 +84,12 @@ public class User
     {
         return this.email;
     }
+    public boolean getAdmin()
+    {
+        return this.admin;
+    }
+
+    /*--- Setter Methods ---*/
     public void setUsername(String userName)
     {
         this.username = userName;
@@ -98,19 +119,21 @@ public class User
         this.email = email;
     }
 
+
     /*--- Class Methods ---*/
     @Override
     public String toString()
     {
         String ret = new String();
         // Convert the class info into a string format
-        ret = String.format("{Username:%1$s}\n{Id:%2$d}\n{Firstname:%3$s}\n{Lastname:%4$s}\n{Email:%5$s}\n{University:%6$s}\n\n",
+        ret = String.format("{Username:%1$s}\n{Id:%2$d}\n{Firstname:%3$s}\n{Lastname:%4$s}\n{Email:%5$s}\n{University:%6$s}\n{Admin:%7$b}",
                             this.username,
                             this.Id,
                             this.firstname,
                             this.lastname,
                             this.email,
-                            this.university
+                            this.university,
+                            this.admin
                             );
         return ret;
     }
--- BackEnd/app/src/main/java/org/campusmarket/db/repositories/AdminsRepository.java
+++
@@ -1,9 +0,0 @@
-package org.campusmarket.db.repositories;
-
-import org.campusmarket.app.models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AdminsRepository extends JpaRepository<User, Integer>
-{
-
-}
