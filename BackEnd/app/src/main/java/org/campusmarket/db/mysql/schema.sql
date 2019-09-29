778665de7c96006d4991a5c419727cc1fb0cd6db
Fadel Alshammasi <fadelalshammasi@Fadels-MacBook-Pro.local>
2019-09-29 15:58:26 -0500

    increased length

--- BackEnd/app/src/main/java/org/campusmarket/db/mysql/schema.sql
+++ BackEnd/app/src/main/java/org/campusmarket/db/mysql/schema.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS users(
     id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    username  VARCHAR(20) NOT NULL,  
+    username  VARCHAR(30) NOT NULL,  
     password   VARCHAR(16) NOT NULL,
-    firstname VARCHAR(20),
-    lastname  VARCHAR(20),
-    email      VARCHAR(30),
+    firstname VARCHAR(30),
+    lastname  VARCHAR(30),
+    email      VARCHAR(45),
     university VARCHAR(30),
     admin     BOOLEAN,
 
