45b0a1cb73879f7d8bdaec3fc08f653880cb79a5
Lily Krohn <Lily@admins-MacBook-Pro-2.local>
2019-09-29 12:46:58 -0500

    Basic validation working for login screen

--- FrontEnd/cMarket/app/src/main/java/com/example/campusmarket/LoginActivity.java
+++ FrontEnd/cMarket/app/src/main/java/com/example/campusmarket/LoginActivity.java
@@ -8,17 +8,38 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class LoginActivity extends AppCompatActivity {
+import com.basgeekball.awesomevalidation.AwesomeValidation;
+import com.basgeekball.awesomevalidation.ValidationStyle;
+import com.google.common.collect.Range;
+
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private AwesomeValidation awesomeValidation;
+    EditText etUsername;
+    EditText etPassword;
+    Button bLogin;
+    TextView registerLink;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        final EditText etUsername = (EditText) findViewById(R.id.etUsername);
-        final EditText etPassword = (EditText) findViewById(R.id.etPassword);
-        final Button bLogin= (Button) findViewById(R.id.bLogin);
-        final TextView registerLink = (TextView) findViewById(R.id.tvRegisterHere);
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+        bLogin = findViewById(R.id.bLogin);
+        registerLink = findViewById(R.id.tvRegisterHere);
+
+
+        awesomeValidation = new AwesomeValidation(ValidationStyle.BASIC);
+
+        awesomeValidation.addValidation(this, R.id.etUsername, "^[A-Za-z\\s]{1,}[\\.]{0,1}[A-Za-z\\s]{0,}$", R.string.userNameError);
+        awesomeValidation.addValidation(this, R.id.etPassword, "^[A-Za-z\\s]{1,}[\\.]{0,1}[A-Za-z\\s]{0,}$", R.string.passwordError);
+
+        // ^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{4,}$ -- this may work better in future
+        
+        bLogin.setOnClickListener(this);
 
         registerLink.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,6 +49,26 @@ public class LoginActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    private void validateForm() {
+        // first validate the form then move ahead
+        // if this becomes true that means validation is successful
+        if (awesomeValidation.validate()) {
+            Intent intent = new Intent(this, UserActivity.class);
+            EditText editText = (EditText) findViewById(R.id.etUsername);
+            String message = editText.getText().toString();
+            intent.putExtra(MainActivity.EXTRA_MESSAGE, message);
+            startActivity(intent);
+            // send the user to the "start a json request" page
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == bLogin) {
+            validateForm();
+        }
     }
 
     // Called when user finishes logging in
