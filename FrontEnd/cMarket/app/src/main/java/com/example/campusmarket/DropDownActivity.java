1bdb3a95889394476d979324cde0f0eca493be88
Katelyn <klamison@iastate.edu>
2019-09-28 20:57:05 -0500

    got json data to return for item names

--- FrontEnd/cMarket/app/src/main/java/com/example/campusmarket/DropDownActivity.java
+++ FrontEnd/cMarket/app/src/main/java/com/example/campusmarket/DropDownActivity.java
@@ -1,21 +1,40 @@
 package com.example.campusmarket;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.example.campusmarket.app.AppController;
+import com.example.campusmarket.utils.Const;
+
+import org.json.JSONArray;
+
 public class DropDownActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    private ProgressDialog pDialog;
+    private String TAG = DropDownActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dropdown_page);
+
+        pDialog = new ProgressDialog(this);
+        pDialog.setMessage("Loading...");
+        pDialog.setCancelable(false);
 
         Spinner spinner = findViewById(R.id.spinner);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.listItems, android.R.layout.simple_spinner_item);
@@ -28,10 +47,57 @@ public class DropDownActivity extends AppCompatActivity implements AdapterView.O
     public void onItemSelected(AdapterView<?> parent, View view, int position, long l) {
         String text = parent.getItemAtPosition(position).toString();
         Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
+
+        // call to make json array
+        String url = "";
+        if (text.equals("Phone"))
+            url = Const.URL_ITEM_CATEGORY + "/" + text;
+        else
+            url = Const.URL_ITEM_NAME + "/" + text;
+
+        TextView msgResponse = findViewById(R.id.itemMsgResponse);
+        makeJsonArryReq(url, msgResponse);
     }
 
     @Override
     public void onNothingSelected(AdapterView<?> adapterView) {
 
+    }
+
+    private void showProgressDialog() {
+        if (!pDialog.isShowing())
+            pDialog.show();
+    }
+
+    private void hideProgressDialog() {
+        if (pDialog.isShowing())
+            pDialog.hide();
+    }
+
+    /**
+     * Making json array request
+     * */
+    private void makeJsonArryReq(String url, final TextView msgResponse) {
+        String tag_json_arry = "jarray_req";
+        showProgressDialog();
+        JsonArrayRequest req = new JsonArrayRequest(url,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        Log.d(TAG, response.toString());
+                        msgResponse.setText(response.toString());
+                        hideProgressDialog();
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d(TAG, "Error: " + error.getMessage());
+                hideProgressDialog();
+            }
+        });
+
+        // Adding request to request queue
+        AppController.getInstance().addToRequestQueue(req,
+                tag_json_arry);
     }
 }
