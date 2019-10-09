9207c600a8da955f1adf70cdd054e0432bb62701
Katelyn <klamison@iastate.edu>
2019-10-09 10:21:32 -0500

    adding dashboard message response

--- FrontEnd/cMarket/app/src/main/java/com/example/campusmarket/DashboardActivity.java
+++ FrontEnd/cMarket/app/src/main/java/com/example/campusmarket/DashboardActivity.java
@@ -2,13 +2,76 @@ package com.example.campusmarket;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.example.campusmarket.app.AppController;
+import com.example.campusmarket.utils.Const;
+
+import org.json.JSONArray;
 
 public class DashboardActivity extends AppCompatActivity {
+
+    private String TAG = DashboardActivity.class.getSimpleName();
+    private ProgressDialog pDialog;
+    private TextView msgResponse;
+    private String  tag_json_arry = "jarray_req";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        msgResponse = (TextView) findViewById(R.id.msgDashboardResponse);
+        pDialog = new ProgressDialog(this);
+        pDialog.setMessage("Loading...");
+        pDialog.setCancelable(false);
+        makeJsonArryReq();
+    }
+
+    private void showProgressDialog() {
+        if (!pDialog.isShowing())
+            pDialog.show();
+    }
+
+    private void hideProgressDialog() {
+        if (pDialog.isShowing())
+            pDialog.hide();
+    }
+
+    /**
+     * Making json array request
+     * */
+    private void makeJsonArryReq() {
+        showProgressDialog();
+        JsonArrayRequest req = new JsonArrayRequest(Const.URL_ITEM_ALL,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        Log.d(TAG, response.toString());
+                        msgResponse.setText(response.toString());
+                        hideProgressDialog();
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d(TAG, "Error: " + error.getMessage());
+                hideProgressDialog();
+            }
+        });
+
+        // Adding request to request queue
+        AppController.getInstance().addToRequestQueue(req,
+                tag_json_arry);
+
+        // Cancelling request
+        // ApplicationController.getInstance().getRequestQueue().cancelAll(tag_json_arry);
     }
 }
