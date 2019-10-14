3f92bdd5e660fc5f0c68f3df986b69d168a33285
Lily Krohn <Lily@lilykrohn.student.iastate.edu>
2019-10-14 11:48:29 -0500

    Adding documentation for posting items

--- FrontEnd/cMarket/app/src/main/java/com/example/campusmarket/NewPostActivity.java
+++ FrontEnd/cMarket/app/src/main/java/com/example/campusmarket/NewPostActivity.java
@@ -4,12 +4,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SeekBar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class NewPostActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button btnSubmitPost;
+    private SeekBar priceBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +20,10 @@ public class NewPostActivity extends AppCompatActivity implements View.OnClickLi
 
         btnSubmitPost = findViewById(R.id.btnSubmitPost);
         btnSubmitPost.setOnClickListener(this);
+//        priceBar.findViewById(R.id.priceBar);
+//        priceBar.setMax(200); // 200 maximum value for the Seek bar
     }
+
 
     @Override
     public void onClick(View v) {
