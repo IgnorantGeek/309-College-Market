26fbe7a764a188b53486db61d15236879f0999bd
Katelyn <klamison@iastate.edu>
2019-10-18 14:40:55 -0500

    autofill edit page with item's information

--- FrontEnd/cMarket/app/src/main/java/com/example/campusmarket/EditPost.java
+++ FrontEnd/cMarket/app/src/main/java/com/example/campusmarket/EditPost.java
@@ -4,24 +4,59 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class EditPost extends AppCompatActivity {
 
+    EditText etName, etPrice, etCondition, etCategory;
+    JSONObject objectToEdit;
+    Button btnSubmit;
 
-    String objectToEdit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_post);
 
 
+        // get the item's current information from the last activity
         Intent intent = getIntent();
-        objectToEdit = intent.getStringExtra("ExtraMessage");
+        String response = intent.getStringExtra("ExtraMessage");
 
-//        // Capture the layout's TextView and set the string as its text
-        TextView textView = findViewById(R.id.textview_edit_post);
-        textView.setText(objectToEdit);
+        // create JSON Object from the response String
+        try {
+            objectToEdit = new JSONObject(response);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        // initialize text box variables
+        etName = findViewById(R.id.etEditName);
+        etPrice = findViewById(R.id.etEditPrice);
+        etCondition = findViewById(R.id.etEditCondition);
+        etCategory = findViewById(R.id.etEditCategory);
+        initializeEditTextFields(objectToEdit);
+
+    }
+
+    /**
+     * Initializes all of the edit text fields with the item's information
+     * @param obj  JSON Object representing the item
+     */
+    private void initializeEditTextFields(JSONObject obj) {
+        try {
+            etName.setText(obj.getString("name"));
+            etPrice.setText(obj.getString("price"));
+            etCondition.setText(obj.getString("condition"));
+            etCategory.setText(obj.getString("category"));
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 }
