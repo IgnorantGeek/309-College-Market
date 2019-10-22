57686cb1a86fc9edcd8aa6b2a9fbbf77b84cdf80
Nick Heisler <nheisler@iastate.edu>
2019-10-21 22:40:42 -0500

    enabled jdbc session security

--- BackEnd/app/src/main/java/org/campusmarket/app/SpringSecurityConfig.java
+++ BackEnd/app/src/main/java/org/campusmarket/app/SpringSecurityConfig.java
@@ -1,52 +1,52 @@
-// package org.campusmarket.app;
+package org.campusmarket.app;
 
-// import java.util.Properties;
+import java.util.Properties;
 
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-// import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-// import org.springframework.security.config.annotation.web.builders.WebSecurity;
-// import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-// import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-// import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-// import org.springframework.security.crypto.password.PasswordEncoder;
-// import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 
-// @EnableWebSecurity
-// @Configuration
-// public class SpringSecurityConfig extends WebSecurityConfigurerAdapter 
-// {
-//     @Override
-//     protected void configure(AuthenticationManagerBuilder auth) throws Exception 
-//     {
-//         auth.userDetailsService(inMemoryUserDetailsManager());
-//     }
+@EnableWebSecurity
+@Configuration
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter 
+{
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception 
+    {
+        auth.userDetailsService(inMemoryUserDetailsManager());
+    }
 
-//     @Override
-//     public void configure(WebSecurity web) throws Exception 
-//     {
-//         web.ignoring().antMatchers("/h2-console/**");
-//     }
+    @Override
+    public void configure(WebSecurity web) throws Exception 
+    {
+        web.ignoring().antMatchers("/h2-console/**");
+    }
 
-//     @Override
-//     public void configure(HttpSecurity http) throws Exception
-//     {
-//         //whatever?
-//     }
+    @Override
+    public void configure(HttpSecurity http) throws Exception
+    {
+        //whatever?
+    }
 
-//     @Bean
-//     public PasswordEncoder passwordEncoder()
-//     {
-//         return new BCryptPasswordEncoder();
-//     }
+    @Bean
+    public PasswordEncoder passwordEncoder()
+    {
+        return new BCryptPasswordEncoder();
+    }
 
-//     @Bean
-//     public InMemoryUserDetailsManager inMemoryUserDetailsManager()
-//     {
-//         final Properties users = new Properties();
-//         users.put("user", "pass,ROLE_USER,enabled");
-//         return new InMemoryUserDetailsManager(users);
-//     }
-// }
+    @Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager()
+    {
+        final Properties users = new Properties();
+        users.put("user", "pass,ROLE_USER,enabled");
+        return new InMemoryUserDetailsManager(users);
+    }
+}
