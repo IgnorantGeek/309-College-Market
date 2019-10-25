ab1d136cfd30661bcd60964ec6e6f66be6af4ef2
Nick Heisler <nheisler@iastate.edu>
2019-10-25 01:42:52 -0500

    tried to update security config, still no login

--- BackEnd/app/src/main/java/org/campusmarket/app/SpringSecurityConfig.java
+++ BackEnd/app/src/main/java/org/campusmarket/app/SpringSecurityConfig.java
@@ -3,14 +3,12 @@ package org.campusmarket.app;
 import org.campusmarket.app.services.CmarketUserDetailsService;
 import org.campusmarket.db.repositories.UsersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableJpaRepositories(basePackageClasses = UsersRepository.class)
@@ -38,9 +36,17 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter
                 .formLogin().permitAll();
     }
 
-    @Bean
-    public PasswordEncoder passwordEncoder()
-    {
-        return new BCryptPasswordEncoder();
+    private PasswordEncoder passwordEncoder() {
+        return new PasswordEncoder() {
+            @Override
+            public String encode(CharSequence charSequence) {
+                return charSequence.toString();
+            }
+
+            @Override
+            public boolean matches(CharSequence charSequence, String s) {
+                return true;
+            }
+        };
     }
 }
