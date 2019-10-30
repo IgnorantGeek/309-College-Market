e9b99dcf5c82d37b505654520b9eebe5e92133f5
Nick Heisler <nheisler@iastate.edu>
2019-10-29 22:35:35 -0500

    updated config

--- BackEnd/app/src/main/java/org/campusmarket/app/Application.java
+++ BackEnd/app/src/main/java/org/campusmarket/app/Application.java
@@ -2,7 +2,9 @@ package org.campusmarket.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories(basePackages = "org.campusmarket.db.repositories")
 @SpringBootApplication
 public class Application
 {
