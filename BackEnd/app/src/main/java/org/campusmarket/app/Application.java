bd298befd8512365aa957bdd7d14f9b452d2f763
Nick Heisler <nheisler@iastate.edu>
2019-11-01 22:40:31 -0500

    added web socket demo code

--- BackEnd/app/src/main/java/org/campusmarket/app/Application.java
+++ BackEnd/app/src/main/java/org/campusmarket/app/Application.java
@@ -2,9 +2,11 @@ package org.campusmarket.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaRepositories(basePackages = "org.campusmarket.db.repositories")
+@ComponentScan(basePackages = "endpoints")
 @SpringBootApplication
 public class Application
 {
