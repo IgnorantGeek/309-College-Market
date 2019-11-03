62fa3f0e6e743576219f2d785d2f63a1f51d8732
Katelyn <klamison@iastate.edu>
2019-11-03 14:12:14 -0600

    create session for log in

--- FrontEnd/cMarket/app/src/main/java/com/example/campusmarket/utils/Const.java
+++ FrontEnd/cMarket/app/src/main/java/com/example/campusmarket/utils/Const.java
@@ -15,4 +15,6 @@ public class Const {
     public static final String URL_ITEM_ALL = "http://coms-309-jr-1.misc.iastate.edu:8080/items/all";
     public static final String URL_NEW_ITEM = "http://coms-309-jr-1.misc.iastate.edu:8080/items/new";
     public static final String URL_ITEM_SELLER = "http://coms-309-jr-1.misc.iastate.edu:8080/items/seller";
+    public static final String URL_SESSION_NEW = "http://coms-309-jr-1.misc.iastate.edu:8080/sessions/new";
+
 }
