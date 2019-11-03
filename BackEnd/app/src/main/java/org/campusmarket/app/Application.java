1c635910f5e891a0759401d10d0a253022be78cd
Nick Heisler <nheisler@iastate.edu>
2019-11-03 13:24:39 -0600

    removed endpoint

--- BackEnd/app/src/main/java/org/campusmarket/app/Application.java
+++ BackEnd/app/src/main/java/org/campusmarket/app/Application.java
@@ -2,7 +2,7 @@ package org.campusmarket.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+//import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 /**
  * A class that has a main method to run the spring application 
@@ -11,7 +11,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
  *
  */
 @EnableJpaRepositories(basePackages = "org.campusmarket.db.repositories")
-@ComponentScan(basePackages = "endpoints")
+// @ComponentScan(basePackages = "endpoints")
 @SpringBootApplication
 public class Application
 {
