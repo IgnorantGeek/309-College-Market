c2373767be290a1910b3cc627e15475085843359
Nick Heisler <nheisler@iastate.edu>
2019-11-03 16:26:12 -0600

    updated js example

--- Documents/Nick-Notes/WebSocketJS/websocket.js
+++ Documents/Nick-Notes/WebSocketJS/websocket.js
@@ -6,7 +6,7 @@ function connect() {
     var host = document.location.host;
     var pathname = document.location.pathname;
     
-    ws = new WebSocket("ws://" +"localhost:8080"+"/chat" + "/"+username);
+    ws = new WebSocket("ws://" +"coms-309-jr-1.misc.iastate.edu:8080"+"/chat" + "/"+username);
 
     ws.onmessage = function(event) {
     var log = document.getElementById("log");
