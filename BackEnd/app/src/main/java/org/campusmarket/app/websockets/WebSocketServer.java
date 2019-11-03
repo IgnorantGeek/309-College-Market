3e7052dd4af34ec6ae3cd03bfa2c8be5fee22c4d
Nick Heisler <nheisler@iastate.edu>
2019-11-03 15:52:16 -0600

    updated socket server

--- BackEnd/app/src/main/java/org/campusmarket/app/websockets/WebSocketServer.java
+++ BackEnd/app/src/main/java/org/campusmarket/app/websockets/WebSocketServer.java
@@ -1,37 +1,21 @@
-<<<<<<< HEAD:BackEnd/app/src/main/java/org/campusmarket/app/endpoints/ChatEndpoint.java
-// package org.campusmarket.app.endpoints;
-=======
 package org.campusmarket.app.websockets;
->>>>>>> 37-lily-websockets:BackEnd/app/src/main/java/org/campusmarket/app/websockets/WebSocketServer.java
 
-// import java.io.IOException;
-// import java.util.HashMap;
-// import java.util.Map;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
-// import javax.websocket.OnClose;
-// import javax.websocket.OnError;
-// import javax.websocket.OnMessage;
-// import javax.websocket.OnOpen;
-// import javax.websocket.Session;
-// import javax.websocket.server.PathParam;
-// import javax.websocket.server.ServerEndpoint;
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.PathParam;
+import javax.websocket.server.ServerEndpoint;
 
-// import org.apache.commons.logging.Log;
-// import org.apache.commons.logging.LogFactory;
-// import org.springframework.stereotype.Component;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.stereotype.Component;
 
-<<<<<<< HEAD:BackEnd/app/src/main/java/org/campusmarket/app/endpoints/ChatEndpoint.java
-// @ServerEndpoint(value = "/chat/{username}")
-// @Component
-// public class ChatEndpoint
-// {
-//     private static Map<Session, String> sessionUsernameMap = new HashMap<>();
-// 	private static Map<String, Session> usernameSessionMap = new HashMap<>();
-	
-
-    
-//     Log logger = LogFactory.getLog(ChatEndpoint.class);
-=======
 @ServerEndpoint(value = "/chat/{username}")
 @Component
 public class WebSocketServer
@@ -42,83 +26,82 @@ public class WebSocketServer
 
     
     Log logger = LogFactory.getLog(WebSocketServer.class);
->>>>>>> 37-lily-websockets:BackEnd/app/src/main/java/org/campusmarket/app/websockets/WebSocketServer.java
     
-//     @OnOpen
-//     public void onOpen(Session session, 
-//     	      		   @PathParam("username") String username) throws IOException 
-//     {
-//         logger.info("Entered into Open");
+    @OnOpen
+    public void onOpen(Session session, 
+    	      		   @PathParam("username") String username) throws IOException 
+    {
+        logger.info("Entered into Open");
         
-//         sessionUsernameMap.put(session, username);
-//         usernameSessionMap.put(username, session);
+        sessionUsernameMap.put(session, username);
+        usernameSessionMap.put(username, session);
         
-//         String message="User:" + username + " has Joined the Chat";
-//         	broadcast(message);
+        String message="User:" + username + " has Joined the Chat";
+        	broadcast(message);
 		
-//     }
+    }
  
-//     @OnMessage
-//     public void onMessage(Session session, String message) throws IOException 
-//     {
-//         // Handle new messages
-//     	logger.info("Entered into Message: Got Message:"+message);
-//     	String username = sessionUsernameMap.get(session);
+    @OnMessage
+    public void onMessage(Session session, String message) throws IOException 
+    {
+        // Handle new messages
+    	logger.info("Entered into Message: Got Message:"+message);
+    	String username = sessionUsernameMap.get(session);
     	
-//     	if (message.startsWith("@")) // Direct message to a user using the format "@username <message>"
-//     	{
-//     		String destUsername = message.split(" ")[0].substring(1); // don't do this in your code!
-//     		sendMessageToPArticularUser(destUsername, "[DM] " + username + ": " + message);
-//     		sendMessageToPArticularUser(username, "[DM] " + username + ": " + message);
-//     	}
-//     	else // Message to whole chat
-//     	{
-// 	    	broadcast(username + ": " + message);
-//     	}
-//     }
+    	if (message.startsWith("@")) // Direct message to a user using the format "@username <message>"
+    	{
+    		String destUsername = message.split(" ")[0].substring(1); // don't do this in your code!
+    		sendMessageToPArticularUser(destUsername, "[DM] " + username + ": " + message);
+    		sendMessageToPArticularUser(username, "[DM] " + username + ": " + message);
+    	}
+    	else // Message to whole chat
+    	{
+	    	broadcast(username + ": " + message);
+    	}
+    }
  
-//     @OnClose
-//     public void onClose(Session session) throws IOException
-//     {
-//     	logger.info("Entered into Close");
+    @OnClose
+    public void onClose(Session session) throws IOException
+    {
+    	logger.info("Entered into Close");
     	
-//     	String username = sessionUsernameMap.get(session);
-//     	sessionUsernameMap.remove(session);
-//     	usernameSessionMap.remove(username);
+    	String username = sessionUsernameMap.get(session);
+    	sessionUsernameMap.remove(session);
+    	usernameSessionMap.remove(username);
         
-//     	String message= username + " disconnected";
-//         broadcast(message);
-//     }
+    	String message= username + " disconnected";
+        broadcast(message);
+    }
  
-//     @OnError
-//     public void onError(Session session, Throwable throwable) 
-//     {
-//         // Do error handling here
-//     	logger.info("Entered into Error");
-//     }
+    @OnError
+    public void onError(Session session, Throwable throwable) 
+    {
+        // Do error handling here
+    	logger.info("Entered into Error");
+    }
     
-// 	private void sendMessageToPArticularUser(String username, String message) 
-//     {	
-//     	try {
-//     		usernameSessionMap.get(username).getBasicRemote().sendText(message);
-//         } catch (IOException e) {
-//         	logger.info("Exception: " + e.getMessage().toString());
-//             e.printStackTrace();
-//         }
-//     }
+	private void sendMessageToPArticularUser(String username, String message) 
+    {	
+    	try {
+    		usernameSessionMap.get(username).getBasicRemote().sendText(message);
+        } catch (IOException e) {
+        	logger.info("Exception: " + e.getMessage().toString());
+            e.printStackTrace();
+        }
+    }
     
-//     private static void broadcast(String message) 
-//     	      throws IOException 
-//     {	  
-//     	sessionUsernameMap.forEach((session, username) -> {
-//     		synchronized (session) {
-// 	            try {
-// 	                session.getBasicRemote().sendText(message);
-// 	            } catch (IOException e) {
-// 	                e.printStackTrace();
-// 	            }
-// 	        }
-// 	    });
-// 	}
+    private static void broadcast(String message) 
+    	      throws IOException 
+    {	  
+    	sessionUsernameMap.forEach((session, username) -> {
+    		synchronized (session) {
+	            try {
+	                session.getBasicRemote().sendText(message);
+	            } catch (IOException e) {
+	                e.printStackTrace();
+	            }
+	        }
+	    });
+	}
 
-// }
+}
