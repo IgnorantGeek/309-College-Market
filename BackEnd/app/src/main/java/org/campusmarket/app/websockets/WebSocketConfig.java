6e93cebc222104085bc74f44ec8a40d0c68df370
Nick Heisler <nheisler@iastate.edu>
2019-11-03 15:54:17 -0600

    fixed backend build error

--- BackEnd/app/src/main/java/org/campusmarket/app/websockets/WebSocketConfig.java
+++ BackEnd/app/src/main/java/org/campusmarket/app/websockets/WebSocketConfig.java
@@ -1,24 +1,9 @@
-<<<<<<< HEAD:BackEnd/app/src/main/java/org/campusmarket/app/endpoints/ChatEndpointConfig.java
-// package org.campusmarket.app.endpoints;
-=======
 package org.campusmarket.app.websockets;
->>>>>>> 37-lily-websockets:BackEnd/app/src/main/java/org/campusmarket/app/websockets/WebSocketConfig.java
 
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
-<<<<<<< HEAD:BackEnd/app/src/main/java/org/campusmarket/app/endpoints/ChatEndpointConfig.java
-// @Configuration
-// public class ChatEndpointConfig
-// {
-//     @Bean
-//     public ServerEndpointExporter serverEndpointExporter()
-//     {
-//         return new ServerEndpointExporter();
-//     }
-// }
-=======
 @Configuration
 public class WebSocketConfig
 {
@@ -28,4 +13,3 @@ public class WebSocketConfig
         return new ServerEndpointExporter();
     }
 }
->>>>>>> 37-lily-websockets:BackEnd/app/src/main/java/org/campusmarket/app/websockets/WebSocketConfig.java
