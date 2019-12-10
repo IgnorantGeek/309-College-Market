ac67e3b29ccced0576499227d57ed9089d0a25e8
Fadel Alshammasi <fadelalshammasi@Fadels-MacBook-Pro.local>
2019-12-09 18:40:31 -0600

    made things private and started breaking up some methods

--- BackEnd/app/src/main/java/org/campusmarket/app/models/services/ItemService.java
+++ BackEnd/app/src/main/java/org/campusmarket/app/models/services/ItemService.java
@@ -1,101 +1,85 @@
-package org.campusmarket.app.models.services;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import org.campusmarket.app.models.Item;
-import org.campusmarket.app.exception.MyFileNotFoundException;
-import org.campusmarket.db.repositories.ItemsRepository;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Service;
-
-
-
-/**
- * A service class to help testing the mockito for items 
- * @author fadelalshammasi
- *
- */
-@Service
-public class ItemService {
-
-	@Autowired
-	 private ItemsRepository repo;
+	package org.campusmarket.app.models.services;
 	
-
-	/**
-	 * A method to get all items 
-	 * @return the list of all items added 
-	 */
-	public List<Item> getAllItemList(){
-						
-	    return repo.findAll();
+	import java.util.ArrayList;
+	import java.util.List;
 	
-	}
+	import org.campusmarket.app.models.Item;
+	import org.campusmarket.app.exception.MyFileNotFoundException;
+	import org.campusmarket.db.repositories.ItemsRepository;
+	
+	import org.springframework.beans.factory.annotation.Autowired;
+	import org.springframework.data.repository.query.Param;
+	import org.springframework.stereotype.Service;
+	
+	
 	
 	/**
-	 * A method to find an item given their refnum 
-	 * @param refnum
-	 * @return the item that has that unique refnum 
+	 * A service class to help testing the mockito for items 
+	 * @author fadelalshammasi
+	 *
 	 */
-	public Item findByRefnum(int refnum) {
-		return repo.findByRefnum(refnum);
+	@Service
+	public class ItemService {
+	
+		@Autowired
+		 private ItemsRepository itemsRepo;
 		
-	}
 	
-	/**
-	 * A method to get all items sold by a specfic seller 
-	 * @param seller
-	 * @return an arraylist of items what were sold by seller 
-	 */
-	public ArrayList<Item>findBySeller( @Param("seller") String seller){
-		 return repo.findBySeller(seller);
-	}
-	
-	
-	/*
-	 public Item storeFile(MultipartFile file) {
-	        String fileName = StringUtils.cleanPath(file.getOriginalFilename());
-
-	        try {
-	            if(fileName.contains("..")) {
-	                throw new FileStorageException("Filename contains invalid path characters " + fileName);
-	            }
-
-	            Item f = new Item(fileName, file.getContentType(), file.getBytes());
-
-	            return repo.save(f);
-	        } catch (IOException ex) {
-	            throw new FileStorageException("Couldn't save file into the db " + fileName + ". Please try again", ex);
-	        }
-	    }
-*/
-	    /**
-	     * A method to search for a file in the db
-	     * @param id
-	     * @return the file found otherwise throwing an exception
-	     */
-	    public Item getFile(int refnum) {
-	    	try {
-	        return repo.findByRefnum(refnum);
-	    	}
-	    	
-	    	catch (MyFileNotFoundException e) {
-	    		throw new MyFileNotFoundException("Couldn't find file with id " + refnum);
-	    	}
-	    }
-	    public ArrayList<Item>findByCategory( @Param("category") String category){
-			 return repo.findByCategory(category);
+		/**
+		 * A method to get all items 
+		 * @return the list of all items added 
+		 */
+		public List<Item> getAllItemList(){
+							
+		    return itemsRepo.findAll();
+		
 		}
-		  
-	    public ArrayList<Item>findByCondAndName( @Param("name") String name, @Param("cond") String cond){
-			 return repo.findByCondAndName(name, cond);
+		
+		/**
+		 * A method to find an item given their refnum 
+		 * @param refnum
+		 * @return the item that has that unique refnum 
+		 */
+		public Item findByRefnum(int refnum) {
+			return itemsRepo.findByRefnum(refnum);
+			
 		}
-	    
-	    
-	}
-
+		
+		/**
+		 * A method to get all items sold by a specfic seller 
+		 * @param seller
+		 * @return an arraylist of items what were sold by seller 
+		 */
+		public ArrayList<Item>findBySeller( @Param("seller") String seller){
+			 return itemsRepo.findBySeller(seller);
+		}
+		
+		
 	
-
+		    /**
+		     * A method to search for a file in the db
+		     * @param id
+		     * @return the file found otherwise throwing an exception
+		     */
+		    public Item getFile(int refnum) {
+		    	try {
+		        return itemsRepo.findByRefnum(refnum);
+		    	}
+		    	
+		    	catch (MyFileNotFoundException e) {
+		    		throw new MyFileNotFoundException("Couldn't find file with id " + refnum);
+		    	}
+		    }
+		    public ArrayList<Item>findByCategory( @Param("category") String category){
+				 return itemsRepo.findByCategory(category);
+			}
+			  
+		    public ArrayList<Item>findByCondAndName( @Param("name") String name, @Param("cond") String cond){
+				 return itemsRepo.findByCondAndName(name, cond);
+			}
+		    
+		    
+		}
+	
+		
+	
