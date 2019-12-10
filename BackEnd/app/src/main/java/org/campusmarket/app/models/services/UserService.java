1971d46fb299e298fba209bcc7f0743ee2396617
Fadel Alshammasi <fadelalshammasi@Fadels-MacBook-Pro.local>
2019-12-10 01:33:47 -0600

    finished refactoring A &more stuff for C&D

--- BackEnd/app/src/main/java/org/campusmarket/app/models/services/UserService.java
+++ BackEnd/app/src/main/java/org/campusmarket/app/models/services/UserService.java
@@ -2,7 +2,6 @@
 	
 	import java.util.List;
 	import org.campusmarket.app.models.User;
-	import org.campusmarket.db.repositories.ItemsRepository;
 	import org.campusmarket.db.repositories.UsersRepository;
 	import org.springframework.beans.factory.annotation.Autowired;
 	import org.springframework.stereotype.Service;
@@ -16,14 +15,14 @@
 	public class UserService
 	{
 	    @Autowired
-	    private UsersRepository repo;
+		private UsersRepository repo;
 	    
 	
 	    /**
 	     * A method to get all users
 	     * @return a list of all users created 
 	     */
-	    public List<User> findAll()
+	   public List<User> findAll()
 	    {					
 		    return repo.findAll();
 	    }
@@ -33,7 +32,7 @@
 	     * @param email
 	     * @return the user that has that unique email
 	     */
-	    public User findByEmail(String email)
+	   public User findByEmail(String email)
 	    {
 	        return repo.findByEmail(email);
 	    }
@@ -48,7 +47,7 @@
 	        return repo.findById(id);
 	    }
 	    
-	    public int existsByUserName(String username) {
+	   public int existsByUserName(String username) {
 	    	return repo.existsByUserName(username);
 	    }
 	    
--- BackEnd/app/src/main/java/org/campusmarket/app/models/services/tests/TestItemsService.java
+++ BackEnd/app/src/main/java/org/campusmarket/app/models/services/tests/TestItemsService.java
@@ -1,4 +1,5 @@
-package org.campusmarket.app;
+
+package org.campusmarket.app.models.services.tests;
 import static org.junit.Assert.*;
 
 
@@ -12,35 +13,24 @@ import java.util.List;
 import org.campusmarket.app.models.Item;
 import org.campusmarket.app.models.services.ItemService;
 import org.campusmarket.app.models.User;
-import org.campusmarket.db.repositories.ItemsRepository;
-import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 
 
 
-public class TestItemsService {
+
+public class TestItemsService extends TestServices {
 	
 	@InjectMocks
 	private ItemService itemService;
 
 	
-	@Mock
-	private ItemsRepository repo;
-	
-	@Before
-	public void init() {
-		MockitoAnnotations.initMocks(this);
-	}
-
 	
 	@Test //test 1 ~FA
 	public void getItemByRefnumTest() {
-		when(repo.findByRefnum(1)).thenReturn(new Item (1,"Introduction to Algorithms",80.00,"Book", "Ok", 
+		when(super.itemsRepo.findByRefnum(1)).thenReturn(new Item (1,"Introduction to Algorithms",80.00,"Book", "Ok", 
 				new User("Fadelsh", "abc123","Fadel","Alshammasi","fadelsh@iastate.edu","isu", false)));
-		Item item=itemService.findByRefnum(1);
+		Item item=super.itemsRepo.findByRefnum(1);
 		
 		assertEquals("Introduction to Algorithms", item.getName());
 		assertEquals(80.00, item.getPrice(),0.01d);
@@ -67,12 +57,12 @@ public class TestItemsService {
 		lst.add(itemThree);
 		lst.add(itemFour);
 		
-		when (repo.findAll()).thenReturn(lst);
+		when (super.itemsRepo.findAll()).thenReturn(lst);
 		
 		List<Item>itemList=itemService.getAllItemList();
 		
 		assertEquals(4,itemList.size());
-		verify (repo, times(1)).findAll();
+		verify (super.itemsRepo, times(1)).findAll();
 		
 
 	}
@@ -94,7 +84,7 @@ public class TestItemsService {
 		lst.add(itemThree);
 	// I don't add itemTwo because the test will find items with SM as seller, not anything else
 			
-		when(repo.findBySeller("SM")).thenReturn(lst);
+		when(super.itemsRepo.findBySeller("SM")).thenReturn(lst);
 		
 		List<Item>itemList=itemService.findBySeller("SM");
 		List<Item>itemList2=itemService.findBySeller("SM"); //not using this list but only wanted to call findBySeller("SM") twice to test verify with times(2)
@@ -104,7 +94,7 @@ public class TestItemsService {
 		assertNotEquals("Fadelsh", itemList.get(0).getUser().getUsername());  //we didn't add the item belong to Fadelsh
 		assertEquals("SM", itemList.get(0).getUser().getUsername()); 
 		assertEquals(itemThree.getUser().getUsername(), itemList.get(1).getUser().getUsername()); //just another way of doing the first argument for itemThird 
-		verify(repo, times(2)).findBySeller("SM"); //was called twice
+		verify(itemsRepo, times(2)).findBySeller("SM"); //was called twice
 		
 	}
 	
@@ -129,7 +119,7 @@ ArrayList <Item> lst= new ArrayList<Item>();
 		lst.add(itemTwo);
 	
 		
-		when(repo.findByCondAndName("xBox", "new")).thenReturn(lst);
+		when(super.itemsRepo.findByCondAndName("xBox", "new")).thenReturn(lst);
 		
 		List<Item>itemList=itemService.findByCondAndName("xBox", "new");
 		List<Item>itemList2=itemService.findByCondAndName("xBox", "new");
@@ -145,7 +135,7 @@ ArrayList <Item> lst= new ArrayList<Item>();
 		assertEquals(itemFour.getCondition(),itemList.get(2).getCondition());
 		assertEquals(itemTwo.getName(),itemList.get(3).getName());
 		assertEquals(itemTwo.getCondition(),itemList.get(3).getCondition());
-		verify(repo,times(3)).findByCondAndName("xBox", "new");
+		verify(super.itemsRepo,times(3)).findByCondAndName("xBox", "new");
 
 
 		
@@ -168,15 +158,16 @@ ArrayList <Item> lst= new ArrayList<Item>();
 		lst.add(itemThree);
 		lst.add(itemFour);
 		
-		when(repo.findByCategory("Video games")).thenReturn(lst);
+		when(super.itemsRepo.findByCategory("Video games")).thenReturn(lst);
 		
 		List<Item>itemList=itemService.findByCategory("Video games");
 		assertEquals(2,itemList.size());
 		assertEquals(itemThree.getCategory(), itemList.get(0).getCategory());
 		assertEquals(itemFour.getCategory(), itemList.get(1).getCategory());
-		verify(repo,times(1)).findByCategory("Video games");
+		verify(super.itemsRepo,times(1)).findByCategory("Video games");
 		
 	}
 	
 	
 }
+
